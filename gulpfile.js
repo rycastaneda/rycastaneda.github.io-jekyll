0055d298e4f01597060f4c4f42cb4f901a877d34
Ryan <castaneda.ry@gmail.com>
2015-10-30 18:27:57 +0800

    update: change port

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-/*
+0/*
 Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
 This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
 The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
@@ -231,7 +231,7 @@ gulp.task('clean', function (cb) {
 // Watch files for changes & reload
 gulp.task('serve', ['styles', 'elements', 'images'], function () {
   browserSync({
-    port: 5000,
+    port: 80,
     notify: false,
     logPrefix: 'PSK',
     snippetOptions: {
